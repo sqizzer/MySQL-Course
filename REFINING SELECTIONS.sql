e0017328e9502092d33d124e3988fa9f5d1c5bab
sqizzer <48286685+sqizzer@users.noreply.github.com>
2023-02-15 16:12:02 +0100

    Delete REFINING SELECTIONS.sql

--- REFINING SELECTIONS.sql
+++
@@ -1,7 +0,0 @@
--- SELECT * FROM books WHERE title LIKE ('%stories%')
--- SELECT title, pages FROM books ORDER BY pages DESC LIMIT 1;
--- SELECT CONCAT(title,' - ', released_year) AS summary FROM books ORDER BY released_year DESC LIMIT 3;
--- SELECT title, author_lname FROM books WHERE author_lname LIKE '% %';
--- SELECT title, released_year, stock_quantity FROM books ORDER BY stock_quantity LIMIT 3
--- SELECT title, author_lname FROM books ORDER BY author_lname,title;
--- SELECT CONCAT('MY FAVORITE AUTHOR IS ', author_fname, ' ', author_lname,'!') AS yell FROM books ORDER BY author_lname;
